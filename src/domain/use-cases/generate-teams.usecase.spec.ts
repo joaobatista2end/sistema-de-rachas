1c6ee9d5c8d3071713fe3871f2bf5f61c047ae9c
João Batista Gomes Silva <jhonxbatista@outlook.com>
2024-07-11 17:32:25 -0300

    chore: orginze test code

--- src/domain/use-cases/generate-teams.usecase.spec.ts
+++ src/domain/use-cases/generate-teams.usecase.spec.ts
@@ -9,18 +9,18 @@ import { playerFactory } from '../factories/player';
 import { Time } from '../object-values/time';
 import { GenerateTeamsUseCase } from './generate-teams.usecase';
 
+const soccerField = new SoccerField({
+  pixKey: uid(),
+  rentalValue: 300,
+});
+const schedule = new Schedule({
+  day: new Date(),
+  startTime: new Time('10:30:00'),
+  finishTime: new Time('16:30:00'),
+});
+
 describe('Gerar times do racha', () => {
   it('should verify if teams are generated correctly', () => {
-    const soccerField = new SoccerField({
-      pixKey: uid(),
-      rentalValue: 300,
-    });
-    const schedule = new Schedule({
-      day: new Date(),
-      startTime: new Time('10:30:00'),
-      finishTime: new Time('16:30:00'),
-    });
-
     const match = new Match({ soccerField, schedule });
     const players = playerFactory(12);
     const players2 = playerFactory(100);
@@ -33,35 +33,14 @@ describe('Gerar times do racha', () => {
   });
 
   it('should verify if teams are balanced correctly', () => {
-    const soccerField = new SoccerField({
-      pixKey: uid(),
-      rentalValue: 300,
-    });
-
-    const schedule = new Schedule({
-      day: new Date(),
-      startTime: new Time('10:30:00'),
-      finishTime: new Time('16:30:00'),
-    });
-
     const match = new Match({ soccerField, schedule });
-
-    const players = playerFactory(30); // Ajuste a quantidade de jogadores conforme necessário
+    const players = playerFactory(30);
     match.players = players;
-
     const teams = GenerateTeamsUseCase.execute(match);
-
-    console.log(`Number of teams: ${teams.length}`);
-    teams.forEach((team, index) => {
-      console.log(`Team ${index + 1}:`);
-      console.log(`Total Stars: ${team.totalStars}`);
-      console.dir(team, { depth: null, colors: true });
-    });
-
     const minStars = Math.min(...teams.map((team) => team.totalStars));
     const maxStars = Math.max(...teams.map((team) => team.totalStars));
-    expect(maxStars - minStars).toBeLessThanOrEqual(1);
 
+    expect(maxStars - minStars).toBeLessThanOrEqual(1);
     expect(teams.length).toBeGreaterThanOrEqual(2);
     for (const team of teams) {
       expect(team.players.length).toBeGreaterThanOrEqual(Team.minPlayers);
