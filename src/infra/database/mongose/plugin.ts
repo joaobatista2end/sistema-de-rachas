20ceeda3ccbc59f487bca5c982d22cc43ad20297
joaosilvabefore <92741701+joaosilvabefore@users.noreply.github.com>
2024-09-03 15:34:19 -0300

    fix: db url

--- src/infra/database/mongose/plugin.ts
+++ src/infra/database/mongose/plugin.ts
@@ -5,7 +5,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 async function mongooseConnector(fastify: FastifyInstance) {
   try {
     const mongoURL =
-      'mongodb://root:password@localhost:27017/soccer-match?authSource=admin';
+      'mongodb://mongo:e07be2c58d4d201d22eb@rachas_db-rachas:27017/sistema-rachas?authSource=admin';
     const options: ConnectOptions = {
       serverSelectionTimeoutMS: 30000, // 30 segundos
     };
