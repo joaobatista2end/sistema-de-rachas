9fea5e9bd913d4f913f4f752a3f33dfddda0021e
joaosilvabefore <92741701+joaosilvabefore@users.noreply.github.com>
2025-01-02 23:36:31 -0300

    refactor: simplify Time class constructor and improve number getter

--- src/domain/object-values/time.ts
+++ src/domain/object-values/time.ts
@@ -1,53 +1,25 @@
-type TimeParam = 'hours' | 'minutes' | 'seconds';
-
 export class Time {
   hours: number;
   minutes: number;
   seconds: number;
 
   constructor(time: string) {
-    const [hours, minutes, seconds] = time.split(':');
-    this.hours = parseInt(hours);
-    this.minutes = parseInt(minutes);
-    this.seconds = parseInt(seconds);
-  }
-
-  // Converte o tempo para o total em segundos, para facilitar as comparações
-  public toSeconds(): number {
-    return this.hours * 3600 + this.minutes * 60 + this.seconds;
-  }
-
-  public add(timeParam: TimeParam, value: number): Time {
-    const totalSeconds =
-      this.toSeconds() +
-      (timeParam === 'hours'
-        ? value * 3600
-        : timeParam === 'minutes'
-        ? value * 60
-        : value);
-    const newHours = Math.floor(totalSeconds / 3600) % 24; // Garantir que horas fiquem no intervalo 0-23
-    const newMinutes = Math.floor((totalSeconds % 3600) / 60);
-    const newSeconds = totalSeconds % 60;
-
-    return new Time(`${newHours}:${newMinutes}:${newSeconds}`);
-  }
-
-  // Compara se o tempo atual é anterior ao tempo passado como parâmetro
-  public isBefore(otherTime: Time): boolean {
-    return this.toSeconds() < otherTime.toSeconds();
-  }
-
-  // Compara se o tempo atual é posterior ao tempo passado como parâmetro
-  public isAfter(otherTime: Time): boolean {
-    return this.toSeconds() > otherTime.toSeconds();
+    const [hours, minutes, seconds] = time.split(':').map(Number);
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
   }
 
   public isEqual(otherTime: Time): boolean {
     return this.toSeconds() === otherTime.toSeconds();
   }
 
-  get number(): number {
-    return this.hours + (this.minutes / 60 + this.seconds / 3600);
+  public get number(): number {
+    const hours = this.hours || 0;
+    const minutes = this.minutes || 0;
+    const seconds = this.seconds || 0;
+
+    return hours + minutes / 60 + seconds / 3600;
   }
 
   toString(): string {
@@ -62,5 +34,9 @@ export class Time {
     const newMinutes = Math.floor((totalSeconds % 3600) / 60);
     const newSeconds = totalSeconds % 60;
     return new Time(`${newHours}:${newMinutes}:${newSeconds}`);
+  }
+
+  private toSeconds(): number {
+    return this.hours * 3600 + this.minutes * 60 + this.seconds;
   }
 }
