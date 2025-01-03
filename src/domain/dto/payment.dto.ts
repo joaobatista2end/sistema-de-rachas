e6cf86fc0b448645425d072b688e37f99b1b6308
joaosilvabefore <92741701+joaosilvabefore@users.noreply.github.com>
2025-01-02 22:05:29 -0300

    feat: add PaymentDto and CreatePaymentDto for payment handling

---
+++ src/domain/dto/payment.dto.ts
@@ -0,0 +1,19 @@
+import { MatchDto } from './match.dto';
+import { UserDto } from './user.dto';
+
+export type PaymentDto = {
+  id: string;
+  amount: number;
+  paymentDate: string;
+  paymentMethod: string;
+  match: Partial<MatchDto>;
+  user: Partial<UserDto>;
+};
+
+export type CreatePaymentDto = {
+  amount: number;
+  paymentDate: Date;
+  paymentMethod: string;
+  match: string;
+  user: string;
+};
