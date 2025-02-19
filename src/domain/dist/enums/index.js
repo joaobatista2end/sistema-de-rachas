ffe437ef894a7dbfda6899b30cd53200cad32e80
joaosilvabefore <92741701+joaosilvabefore@users.noreply.github.com>
2025-02-19 11:31:46 -0300

    fix

--- src/domain/dist/enums/index.js
+++ src/domain/dist/enums/index.js
@@ -15,4 +15,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./http-status-code"), exports);
+__exportStar(require("./player-position"), exports);
 __exportStar(require("./user-role"), exports);
